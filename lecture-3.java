8d32400f3a8eba05f2ae5ca174baa7cdb0af9833
Ashutosh Pradhan <ashutoshpradhan808@gmail.com>
2024-06-11 08:51:32 +0530

    premitive data types in java

---
+++ lecture-3.java
@@ -0,0 +1,36 @@
+
+class Premitive{
+    public static void main(String[] args){
+        boolean flag = true;
+        System.out.println(flag); 
+
+        byte range;
+        range = 124;
+        System.out.println(range);
+
+        short temperature;
+        temperature = -200;
+        System.out.println(temperature);
+
+        int range1 = -4250000;
+        System.out.println(range1);
+
+        long range2 = -42332200000L;
+        System.out.println(range2);
+
+        double number = -42.3;
+        System.out.println(number);
+
+        float number1 = -42.3f;
+        System.out.println(number1);
+
+        char letter = '\u0051';
+        System.out.println(letter);
+
+        char letter1 = '9';
+        System.out.println(letter1);  
+    	
+        char letter2 = 65;
+        System.out.println(letter2); 
+    }
+}
